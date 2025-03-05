bdc5771973694d52e2eca2d1eeced3858a21939c
MbabaziOwen <163897840+MbabaziOwen@users.noreply.github.com>
2025-03-05 22:05:53 +0300

    polished version

--- dice project/dice.py
+++ dice project/dice.py
@@ -2,11 +2,16 @@
 import random
 print("Hello Player")
 
-#generate a random integer
-random_integer = random.randint(1, 6)
+#Ask for user input 
+user_input = input(str("Enter D to output random number from Dice "))
+if user_input == "d":
+    #generate a random integer
+    random_integer = random.randint(1, 6)
+    #print generated float
+    print(random_integer) 
 
-#print generated float
-print(random_integer)
+else:
+    print('enter D for random number')
 
 
 
