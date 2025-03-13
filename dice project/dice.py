84bbbd315010666d564cd6a1593abcb84642f67f
MbabaziOwen <163897840+MbabaziOwen@users.noreply.github.com>
2025-03-13 08:32:12 +0300

    while loop implemented

--- dice project/dice.py
+++ dice project/dice.py
@@ -4,14 +4,21 @@ print("Hello Player")
 
 #Ask for user input 
 user_input = input(str("Enter D to output random number from Dice "))
-if user_input == "d":
-    #generate a random integer
-    random_integer = random.randint(1, 6)
-    #print generated float
-    print(random_integer) 
 
-else:
-    print('enter D for random number')
-
-
+#while loop (infinite)
+while True:
+    if user_input == "d":
+        #generate a random integer
+        random_dice = random.randint(1,6)
+        print(random_dice)
+        break
+            
+    if user_input == "d":
+        #generate a random integer
+        random_integer = random.randint(1, 6)
+        #print generated float
+        print(random_integer) 
+        if user_input =="e":
+            print("ending game..")
+            break
 
