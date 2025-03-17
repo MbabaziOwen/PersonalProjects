1019136001b9c4037a7000da7b941a8eddd92093
MbabaziOwen <163897840+MbabaziOwen@users.noreply.github.com>
2025-03-17 13:12:04 +0300

    dice 2.o
    dice 2.o

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,47 +1,70 @@
-# Function to print dice faces
-def print_dice(number):
-    if number == 1:
-        print(" ----- ")
-        print("|     |")
-        print("|  o  |")
-        print("|     |")
-        print(" ----- ")
-    elif number == 2:
-        print(" ----- ")
-        print("| o   |")
-        print("|     |")
-        print("|   o |")
-        print(" ----- ")
-    elif number == 3:
-        print(" ----- ")
-        print("| o   |")
-        print("|  o  |")
-        print("|   o |")
-        print(" ----- ")
-    elif number == 4:
-        print(" ----- ")
-        print("| o o |")
-        print("|     |")
-        print("| o o |")
-        print(" ----- ")
-    elif number == 5:
-        print(" ----- ")
-        print("| o o |")
-        print("|  o  |")
-        print("| o o |")
-        print(" ----- ")
-    elif number == 6:
-        print(" ----- ")
-        print("| o o |")
-        print("| o o |")
-        print("| o o |")
-        print(" ----- ")
-    else:
-        print("Invalid dice number!")
-
-# Example usage
+#Documentation
+#this version of the dice game not only provides the random number produced, but also provides a diagram of it 
+#import random 
 import random
 
-dice_roll = random.randint(1, 6)  # Simulating a dice roll
-print(f"You rolled a {dice_roll}:")
-print_dice(dice_roll)
+#function which produces a random between 1 and 6
+def dice(user_input):
+    if user_input == "d":
+            #generate a random integer
+            random_dice = random.randint(1,6)
+            return random_dice; 
+    
+           
+          
+
+print("Hello Player")
+while True:
+    user_input = input(str("Enter D to output random number from Dice "))
+    #break incase Anything but D is pressed 
+    if user_input != "d":
+         break
+    system_output = dice(user_input)
+    print(f"You rolled a",system_output)
+    if system_output == 1:
+         print("-------")
+         print("|     |")
+         print("|  o  |")
+         print("|     |")
+         print("-------")
+    elif system_output == 2:
+         print("-------")
+         print("|o    |")
+         print("|     |")
+         print("|    o|")
+         print("-------")
+    elif system_output == 3:
+         print("-------")
+         print("|o    |")
+         print("|  o  |")
+         print("|    o|")
+         print("-------")
+    elif system_output == 4:
+         print("-------")
+         print("|o   o|")
+         print("|     |")
+         print("|o   o|")
+         print("-------")
+    elif system_output == 5:
+         print("-------")
+         print("|o   o|")
+         print("|  o  |")
+         print("|o   o|")
+         print("-------")
+    elif system_output == 6:
+         print("-------")
+         print("|o   o|")
+         print("|o   o|")
+         print("|o   o|")
+         print("-------")
+    else:
+         print("invalid dice number")
+    
+
+    
+
+
+
+
+
+
